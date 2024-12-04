d3d311009435bf81c1b40dac9ed73d095e578335
Ankit Kokane <ankitkokane90@gmail.com>
2024-12-04 11:03:08 +0530

    added fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "brice-light": ["Brice Light", "sans-serif"],
+        "brice-regular": ["Brice Regular", "sans-serif"],
+        "brice-semibold": ["Brice SemiBold", "sans-serif"],
+        "brice-black": ["Brice Black", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
